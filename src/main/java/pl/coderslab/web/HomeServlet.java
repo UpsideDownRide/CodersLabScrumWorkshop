af650b96d7ed7ac79732c05d35acbfc41e8d9455
kacper <Kacper.tuszynski93@gmail.com>
2022-07-18 13:50:44 +0200

    KT DayName

--- src/main/java/pl/coderslab/web/HomeServlet.java
+++ src/main/java/pl/coderslab/web/HomeServlet.java
@@ -1,7 +1,7 @@
 package pl.coderslab.web;
 
-import pl.coderslab.dao.BookDao;
-import pl.coderslab.model.Book;
+import pl.coderslab.dao.DayNameDao;
+import pl.coderslab.model.DayName;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,9 +19,9 @@ import java.util.List;
 public class HomeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookDao bookDao = new BookDao();
-        List<Book> books = bookDao.findAll();
-        System.out.println(books);
+        DayNameDao dayNameDao = new DayNameDao();
+        List<DayName> dayNames = dayNameDao.findAll();
+        System.out.println(dayNames);
 
         getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
     }
