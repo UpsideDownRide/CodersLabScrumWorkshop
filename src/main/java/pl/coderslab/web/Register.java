48a00e23915ff905d2df3f57d9389ff8b59873fc
golebiewskii.kacper@gmail.com <golebiewskii.kacper@gmail.com>
2022-07-18 15:03:34 +0200

    Registration_Page

--- src/main/java/pl/coderslab/web/Register.java
+++ src/main/java/pl/coderslab/web/Register.java
@@ -5,11 +5,12 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "Register", value = "/Register")
+@WebServlet(name = "Register", value = "/register")
 public class Register extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getServletContext().getRequestDispatcher("/registration.jsp").forward(request,response);
+        response.setContentType("text/html;charset=utf-8");
     }
 
     @Override
