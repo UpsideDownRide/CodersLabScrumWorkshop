e1f5d56d5c99a5ced8792cb6812d4a773a5f784b
Marek Roszak <roszak89@gmail.com>
2022-07-19 10:18:48 +0200

    formularz dodawania planu

---
+++ src/main/java/pl/coderslab/web/PlanAdd.java
@@ -0,0 +1,44 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.model.Plan;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Date;
+
+
+
+@WebServlet(name = "PlanAdd", value = "/app/plan/add")
+public class PlanAdd extends HttpServlet {
+	
+	@Override
+	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/addSchedules.jsp").forward(request,response);
+		
+		
+	}
+	
+	@Override
+	protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String planName = request.getParameter("name");
+		String planDescription = request.getParameter("description");
+		long millis =System.currentTimeMillis();
+		Date date = new Date(millis);
+		
+		Plan plan = new Plan();
+		
+		plan.setName(planName);
+		plan.setDescription(planDescription);
+		plan.setCreated(date);
+		plan.setAdminId(1);
+		
+		PlanDao planDao = new PlanDao();
+		planDao.create(plan);
+		
+		
+	}
+}
